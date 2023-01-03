d4fae199361a7eb22c328a17a9a4996c97a7ec6f
Kiên Nguyễn <trungkien2691998@outlook.com>
2023-01-03 20:19:39 +0700

    Initiate project

---
+++ js/spinner-image.js
@@ -0,0 +1,259 @@
+function initiateWheel(containerId, data, angleRotation) {
+    var padding = { top: 20, right: 40, bottom: 0, left: 0 },
+        // w = 500 - padding.left - padding.right,
+        // h = 500 - padding.top - padding.bottom,
+        w = 700 - padding.left - padding.right,
+        h = 700 - padding.top - padding.bottom,
+        r = Math.min(w, h) / 2,
+        rotation = 0,
+        oldrotation = angleRotation,
+        picked = 100000,
+        oldpick = [],
+        color = d3.scale.category20();
+
+    var svg = d3.select(`#${containerId}`)
+        .append("svg")
+        .data([data])
+        .attr("width", w + padding.left + padding.right)
+        .attr("height", h + padding.top + padding.bottom)
+        .attr("style", "margin-left:50%;transform:translateX(-50%)");
+    var container = svg.append("g")
+        .attr("class", "chartholder")
+        .attr("transform", "translate(" + (w / 2 + padding.left) + "," + (h / 2 + padding.top) + ")");
+    var vis = container
+        .append("g");
+
+    var pie = d3.layout.pie().sort(null).value(function (d) { return 1; });
+    // declare an arc generator function
+    var arc = d3.svg.arc().outerRadius(r);
+    // select paths, use arc generator to draw
+    var arcs = vis.selectAll("g.slice")
+        .data(pie)
+        .enter()
+        .append("g")
+        .attr("class", "slice");
+
+    arcs.append("path")
+        .attr("fill", function (d, i) { return color(i); })
+        .attr("stroke", "white")
+        .attr("stroke-width", 2)
+        .attr("d", function (d) { return arc(d); });
+    // // add the text
+    // arcs.append("text").attr("transform", function (d) {
+    //     d.innerRadius = 0;
+    //     d.outerRadius = r;
+    //     d.angle = (d.startAngle + d.endAngle) / 2;
+    //     return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")translate(" + (d.outerRadius - 10) + ")";
+    // })
+    //     .attr("text-anchor", "end")
+    //     .text(function (d, i) {
+    //         return data[i].text;
+    //     });
+    // add image
+    arcs.append("svg:image")
+        .attr("transform", function (d) {
+            d.innerRadius = 0;
+            d.outerRadius = r;
+            d.angle = (d.startAngle + d.endAngle) / 2;
+            // return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")translate(" + (d.outerRadius - 100) + ", "  + (d.outerRadius - 255) + ")";
+            return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")translate(" + (d.outerRadius - 165) + ", " + (d.outerRadius - 378) + ")";
+        })
+        // .attr("width", "50")
+        // .attr("height", "50")
+        .attr("width", "100")
+        .attr("height", "100")
+        .attr("preserveAspectRatio", "xMidYMid meet")
+        .attr("xlink:href", function (d, i) {
+            return data[i].image;
+        });
+
+    //make arrow
+    svg.append("g")
+        .attr("transform", "translate(" + (w + padding.left + padding.right - 10) + "," + ((h / 2) + padding.top) + ")")
+        .append("path")
+        .attr("d", "M-" + (r * .15 + 5) + ",0L0," + (r * .05 + 5) + "L0,-" + (r * .05 + 5) + "Z")
+        .attr("stroke", "black")
+        .style({ "fill": "#ffc90e" });
+    //draw spin circle
+    container.append("circle")
+        .attr("cx", 0)
+        .attr("cy", 0)
+        .attr("r", 40)
+        .attr("id", "spin-button")
+        .attr("stroke", "white")
+        .attr("stroke-width", 2)
+        .style({ "fill": "rgb(61 61 61)", "cursor": "pointer" });
+    //spin text
+    container.append("text")
+        .attr("x", 0)
+        .attr("y", '0.45vw')
+        .attr("text-anchor", "middle")
+        .text("QUAY")
+        .style({ "font-weight": "bold", "font-size": "20px", "fill": "white" });
+
+    d3.select("#spin-button").on('click', () => {
+        container.on("click", null);
+        //all slices have been seen, all done
+        // console.log("OldPick: " + oldpick.length, "Data length: " + data.length);
+        if (oldpick.length == data.length) {
+            console.log("done");
+            container.on("click", null);
+            return;
+        }
+        var ps = 360 / data.length,
+            pieslice = Math.round(1440 / data.length),
+            // rng = Math.floor((Math.random() * 1440) + 360);
+            // rng = Math.floor(((random(1, 4) / 10 + 5) * 1440) + 360);
+            // rng = Math.floor(((Math.random() + 2) * 360) + 360);
+            rng = Math.floor(((Math.random() + 10) * 360) + 360);
+
+        rotation = (Math.round(rng / ps) * ps);
+
+        picked = Math.round(data.length - (rotation % 360) / ps);
+        picked = picked >= data.length ? (picked % data.length) : picked;
+        if (oldpick.indexOf(picked) !== -1) {
+            d3.select(this).call(spin);
+            return;
+        } else {
+            oldpick.push(picked);
+        }
+        rotation += 90 - Math.round(ps / 2);
+
+        // play the audio
+        var audio = new Audio('./audio/spin.mp3');
+        audio.volume = 0.4;
+        audio.play();
+
+        vis.transition()
+            .duration(3000)
+            // .attrTween("transform", rotTween)
+            .attrTween("transform", () => {
+                return function (t) {
+                    var i = d3.interpolate(oldrotation % 360, rotation);
+                    return "rotate(" + i(t) + ", 0, 0)";
+                };
+            })
+            .each("end", function () {
+                // mute the audio
+                audio.muted = true;
+                // mark segment as seen
+                d3.select(".slice:nth-child(" + (picked + 1) + ") path").attr("fill", "#111");
+                // make image as seen
+                d3.select(".slice:nth-child(" + (picked + 1) + ") image").attr("opacity", "0.3");
+
+                oldrotation = rotation;
+
+                // /* Get the result value from object "data" */
+                // console.log(data[picked])
+                Swal.fire({
+                    title: "<i>Người được chọn</i>",
+                    html: `<div style="display: flex;flex-direction: column;align-items: center;justify-content: center;">
+                        <img src="${data[picked].image}" />
+                        <span>${data[picked].text}</span>
+                    <div/>`,
+                    confirmButtonText: "Đóng",
+                });
+
+                // /* Comment the below line for restrict spin to sngle time */
+                // container.on("click", spin);
+                d3.select("#spin-button").on('click', spin);
+            });
+    });
+
+    // Set angle rotation
+    vis.attr("transform", `rotate(${angleRotation})`);
+
+
+    function spin(d) {
+        container.on("click", null);
+        // all slices have been seen, all done
+        if (oldpick.length == data.length) {
+            console.log("done");
+            container.on("click", null);
+            return;
+        }
+        var ps = 360 / data.length,
+            pieslice = Math.round(1440 / data.length),
+            // rng = Math.floor((Math.random() * 1440) + 360);
+            // rng = Math.floor(((random(1, 4) / 10 + 5) * 1440) + 360);
+            // rng = Math.floor(((Math.random() + 2) * 360) + 360);
+            rng = Math.floor(((Math.random() + 10) * 360) + 360);
+
+        rotation = (Math.round(rng / ps) * ps);
+
+        picked = Math.round(data.length - (rotation % 360) / ps);
+        picked = picked >= data.length ? (picked % data.length) : picked;
+        if (oldpick.indexOf(picked) !== -1) {
+            d3.select(this).call(spin);
+            return;
+        } else {
+            oldpick.push(picked);
+        }
+        rotation += 90 - Math.round(ps / 2);
+
+        // play the audio
+        var audio = new Audio('../audio/spin.mp3');
+        audio.volume = 0.4;
+        audio.play();
+
+        vis.transition()
+            .duration(3000)
+            // .attrTween("transform", rotTween)
+            .attrTween("transform", () => {
+                return function (t) {
+                    var i = d3.interpolate(oldrotation % 360, rotation);
+                    return "rotate(" + i(t) + ", 0, 0)";
+                };
+            })
+            .each("end", function () {
+                // mute the audio
+                audio.muted = true;
+                // mark segment as seen
+                d3.select(".slice:nth-child(" + (picked + 1) + ") path").attr("fill", "#111");
+                // make image as seen
+                d3.select(".slice:nth-child(" + (picked + 1) + ") image").attr("opacity", "0.3");
+
+                oldrotation = rotation;
+
+                // /* Get the result value from object "data" */
+                // console.log(data[picked])
+                Swal.fire({
+                    title: "<i>Người được chọn</i>",
+                    html: `<div style="display: flex;flex-direction: column;align-items: center;justify-content: center;">
+                        <img src="${data[picked].image}" />
+                        <span>${data[picked].text}</span>
+                    <div/>`,
+                    confirmButtonText: "Đóng",
+                });
+
+                // /* Comment the below line for restrict spin to sngle time */
+                // container.on("click", spin);
+                d3.select("#spin-button").on('click', spin);
+            });
+    }
+}
+
+function rotTween(to) {
+    var i = d3.interpolate(oldrotation % 360, rotation);
+    return function (t) {
+        return "rotate(" + i(t) + ")";
+    };
+}
+
+
+function getRandomNumbers() {
+    var array = new Uint16Array(1000);
+    var scale = d3.scale.linear().range([360, 1440]).domain([0, 100000]);
+    if (window.hasOwnProperty("crypto") && typeof window.crypto.getRandomValues === "function") {
+        window.crypto.getRandomValues(array);
+        console.log("works");
+    } else {
+        //no support for crypto, get crappy random numbers
+        for (var i = 0; i < 1000; i++) {
+            array[i] = Math.floor(Math.random() * 100000) + 1;
+        }
+    }
+    return array;
+}
+
+const random = (min, max) => Math.random() * (max - min) + min;
---
+++ js/spinner.js
@@ -0,0 +1,75 @@
+function initiateWheel(segmentData, duration, spins, fireworkElement, btnWheelElement) {
+    var theWheel = new Winwheel({
+        'numSegments': segmentData.length,
+        'textAlignment': 'center',
+        'fillStyle': 'silver',     // The segment background colour.
+        'strokeStyle': 'white',
+        'lineWidth': 2,
+        'textFontSize': 18,
+        'rotationAngle': 45,
+        // 'pointerAngle': 45, // Define the position for pointer to get value
+        'responsive': true,
+        'segments': segmentData,
+        'animation':                   // Note animation properties passed in constructor parameters.
+        {
+            'type': 'spinToStop',  // Type of animation.
+            'duration': duration,             // How long the animation is to take in seconds.
+            'spins': spins,              // The number of complete 360 degree rotations the wheel is to do.
+            'callbackSound': playSound,    // Specify function to call when sound is to be triggered.
+            'soundTrigger': 'pin',         // Pins trigger the sound for this animation.
+            'callbackFinished': () => {
+                // Get the audio with the sound it in, then play.
+                let winsound = document.getElementById('winsound');
+                winsound.play();
+
+                // Call getIndicatedSegment() function to return pointer to the segment pointed to on wheel.
+                let winningSegment = theWheel.getIndicatedSegment();
+
+                // Display firework
+                document.getElementById(fireworkElement).style.display = 'block';
+
+                // Basic sweet alert of the segment text which is the prize name.
+                Swal.fire({
+                    title: "<i>Phần thưởng</i>",
+                    html: winningSegment.text,
+                    confirmButtonText: "Đóng",
+                }).then(() => {
+                    document.getElementById(fireworkElement).style.display = 'none';
+                    let findItem = theWheel.segments.filter(item => item != null && item != undefined
+                        && item.text === winningSegment.text && item.fillStyle === winningSegment.fillStyle)[0];
+                    theWheel.deleteSegment(theWheel.segments.indexOf(findItem));
+                    theWheel.rotationAngle -= spins * 360;
+
+                    theWheel.pins.number = theWheel.segments.length * 2;
+                    theWheel.draw();
+                });
+            },
+        },
+        'pins':    // Display pins, and if desired specify the number.
+        {
+            'number': segmentData.length * 2,
+            'outerRadius': 4,
+            'fillStyle': 'white',   // Fill colour of the pins.
+            'strokeStyle': 'black',
+            'responsive': true, // This must be set to true if pin size is to be responsive.
+        }
+    });
+
+    // Trigger for button run wheel
+    document.getElementById(btnWheelElement).setAttribute("onclick", "theWheel.startAnimation();");
+
+    return theWheel;
+}
+
+function playSound() {
+    var audio = new Audio('../audio/tick.mp3');  // Create audio object and load desired file.
+
+    // Stop and rewind the sound (stops it if already playing).
+    audio.pause();
+    audio.currentTime = 0;
+
+    // Play the sound.
+    audio.play();
+}
+
+const random = (min, max) => Math.random() * (max - min) + min;
